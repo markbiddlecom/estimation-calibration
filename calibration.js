c996ea33d8f4de74a51b982d364c9dec64b020f2
Mark Biddlecom <markbiddlecom@gmail.com>
2015-01-07 11:51:32 -0800

    Initial commit.

---
+++ calibration.js
@@ -0,0 +1,242 @@
+// State
+var batches = [];
+var batch = -1;
+
+// Wire up UI events.
+$("#btn-get-started").click(function() {
+	$("#jumbotron").hide("fast");
+	$("#questions-container").removeClass("hidden");
+	prepareRound();
+});
+
+$("#btn-score").click(function() {
+	var empty = false;
+	$("#questions INPUT").each(function(i, e) {
+		$e = $(e);
+		if(!isValidNumberString($e.val())) {
+			$e.parent().addClass("has-error");
+			empty = true;
+		} else {
+			$e.parent().removeClass("has-error");
+		}
+	});
+	if(!empty) {
+		var stats = score(batches[batch], $("#questions .min-answer INPUT"), $("#questions .max-answer INPUT"));
+		$("#questions .question").each(function(i, e) {
+			var answer = stats.answers[i];
+			var $question = $(e);
+
+			$question.addClass(answer.correct ? "correct" : "incorrect");
+			$question.find(".your-answer").html(
+				formatAnswerNumber(answer.guess.min) +
+				" to " + 
+				formatAnswerNumber(answer.guess.max)
+			);
+		});
+
+		$("#questions .result").hide().removeClass("hidden").show("fast");
+		$("#questions .answer").hide("fast");
+
+		var $note = null;
+		if(stats.score < 0.9) { $note = $("#overconfident"); }
+		else if(stats.score > 0.9) { $note = $("#underconfident"); }
+		else { $note = $("#nice"); }
+
+		$note.hide().removeClass("hidden").show("fast");
+		$("#actions").hide().removeClass("hidden").show("fast");
+		$("#btn-score").hide();
+	}
+});
+
+$("#btn-next").click(function() {
+	$("#actions, .note").hide("fast");
+	prepareRound();
+});
+
+$(".answer INPUT").blur(function() {
+	var $this = $(this);
+	$this.val($this.val().replace(/\,/, ""));
+
+	if(!isValidNumberString($this.val())) {
+		$this.parent().addClass("has-error");
+	} else {
+		$this.parent().removeClass("has-error");
+	}
+});
+
+// Initialize
+$(function() {
+	// Load our question batches.
+	var questions = loadQuestions();
+	shuffle(questions.questions);
+	for(var i = 0; i < questions.questions.length; i += 10) {
+		var b = [];
+		for(var j = i; j < i + 10; j++) {
+			b.push(questions.questions[j]);
+		}
+		batches.push(b);
+	}
+});
+
+// Helpers
+function prepareRound() {
+	batch++;
+
+	if(batch >= batches.length) {
+		allDone();
+	}
+	else {
+		var srcCount = 1;
+		var $questionsTable = $("#questions");
+		var $questionTemplate = $("#question-template");
+		$questionsTable.empty();
+		$questionsTable.append($("#questions-header-template").clone().attr("id", ""));
+		for(var i = 0; i < batches[batch].length; i++) {
+			var $q = $($questionTemplate.clone(true, true).attr("id", ""));
+			var q = batches[batch][i];
+			$q.find(".question-text").html(q.q);
+			$q.find(".correct-answer").html(formatAnswerNumber(q.a));
+
+			$questionsTable.append($q);
+
+			var srcHtml = "", src = q.src;
+			if(typeof src === "string") { src = [ src ]; }
+			for(var j = 0; j < src.length; j++) {
+				if(srcHtml.length !== 0) { srcHtml += ", "; }
+				srcHtml += "[" + "<a target='_blank' href='" + src [j]+ "'>" + (srcCount++) + "</a>]";
+			}
+			$q.find(".source").html(srcHtml);
+		}
+		$("#btn-score").show();
+	}
+}
+
+function allDone() {
+	$("#questions-container").hide("fast");
+	$("#all-done").hide().removeClass("hidden").show("fast");
+}
+
+function shuffle(array) {
+	for(var i = 0; i < array.length; i++) {
+		var tmp = array[i];
+		var j = Math.floor(Math.random() * array.length);
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+	return array;
+}
+
+function score(questions, $aAnswers, $bAnswers) {
+	var stats = { correct: 0, answers: [] };
+	for(var i = 0; i < questions.length; i++) {
+		var a = parseFloat($($aAnswers[i]).val());
+		var b = parseFloat($($bAnswers[i]).val());
+		answer(stats, questions[i], a, b);
+	}
+	stats.score = stats.correct / stats.answers.length;
+	return stats;
+}
+
+function answer(stats, question, a, b) {
+	var min = Math.min(a, b);
+	var max = Math.max(a, b);
+	var ans = {
+		question: question,
+		guess: { min: min, max: max },
+		correct: min <= question.a && max >= question.a
+	};
+	stats.answers.push(ans);
+	stats.answers.correct += ans.correct ? 1 : 0;
+	return ans;
+}
+
+function formatAnswerNumber(number) {
+	if("" + number === "" + Math.round(number)) {
+		return formatNumber(number, ",0");
+	}
+	else {
+		var count = ("" + number).replace(/^.*\./, "").length;
+		var format = ",0.";
+		for(var i = 0; i < count && i < 5; i++) { format += "0"; }
+		return formatNumber(number, format);
+	}
+}
+
+function isValidNumberString(n) {
+	return /^[Ee0-9\,\.\+\-]+$/.test(n) && !isNaN(parseFloat(n));
+}
+
+/**
+ * Formats a number according to a format specifier.
+ * https://gist.github.com/markbiddlecom/5079340
+ * 
+ *   [LEFT][.RIGHT]
+ *   LEFT := [,]0+
+ *   RIGHT := 0+
+ */
+function formatNumber(number, format) {
+    if(number === null || number === undefined) { return ""; }
+    if(!(/^\+?(,?0+)?(\.0+)?$/.test(format))) { format = ",0.0"; }
+    
+    if(typeof number === "string") { number = parseFloat(number); }
+    if(typeof number !== "number") { number = 0; }
+    
+    var alwaysSign = format.charAt(0) === '+';
+    if(alwaysSign === true) { format = format.substring(1); }
+    var digitGrouping = format.charAt(0) === ',';
+    if(digitGrouping === true) { format = format.substring(1); }
+    
+    var left, right;
+    var dotPos = format.indexOf(".");
+    if(dotPos == -1) { left = format; right = ""; }
+    else { left = format.substring(0, dotPos); right = format.substring(dotPos + 1); }
+    
+    if(right !== "") {
+        var m = parseInt(padRight("1", "0", right.length + 1));
+        number = Math.round(number * m) / m;
+    }
+    else {
+        number = Math.round(number);
+    }
+    
+    var sNumber = number.toString();
+    if(sNumber.indexOf("e") !== -1) { return sNumber; } // unsupported
+    
+    var nLeft, nRight;
+    dotPos = sNumber.indexOf(".");
+    if(dotPos == -1) { nLeft = sNumber; nRight = ""; }
+    else { nLeft = sNumber.substring(0, dotPos); nRight = sNumber.substring(dotPos + 1); }
+    
+    if(nLeft.charAt(0) === '-') { alwaysSign = true; nLeft = nLeft.substring(1); }
+    if(nLeft.length < left.length) { nLeft = padLeft(nLeft, "0", left.length); }
+    if(nRight.length < right.length) { nRight = padRight(nRight, "0", right.length); }
+    
+    if(digitGrouping === true) {
+        for(var i = nLeft.length - 4; i >= 0; i -= 3) {
+            nLeft = nLeft.substring(0, i + 1) + "," + nLeft.substring(i + 1);
+        }
+    }
+    
+    if(alwaysSign === true) { nLeft = (number < 0 ? "-" : "+") + nLeft; }
+    if(right.length > 0) { nRight = "." + nRight; }
+    
+    return nLeft + nRight;
+}
+
+function padLeft(string, padCharacter, minLength) {
+    if(string === null || string === undefined) { return string; }
+    if(minLength === null || minLength === undefined) { minLength = 0; }
+    else if(typeof minLength !== "number") { minLength = parseInt(minLength.toString()); }
+    string += "";
+    while(string.length < minLength) { string = padCharacter + string; }
+    return string;
+}
+ 
+function padRight(string, padCharacter, minLength) {
+    if(string === null || string === undefined) { return string; }
+    if(minLength === null || minLength === undefined) { minLength = 0; }
+    else if(typeof minLength !== "number") { minLength = parseInt(minLength.toString()); }
+    string += "";
+    while(string.length < minLength) { string += padCharacter; }
+    return string;
+}
---
+++ questions.js
@@ -0,0 +1,257 @@
+function loadQuestions() {
+	return {
+		"version": "1.0",
+		"questions": [
+			{
+				"q": "In what year was construction of the Eiffel Tower completed?",
+				"a": 1889,
+				"src": "http://en.wikipedia.org/wiki/Eiffel_Tower"
+			},
+			{
+				"q": "How many total floors (including basement and maintenance floors) are there in the Burj Khalifia, the largest structure in the world as of 2014?",
+				"a": 211,
+				"src": "http://en.wikipedia.org/wiki/Burj_Khalifa"
+			},
+			{
+				"q": "As of 2014, how many of the world's 10 tallest manmade structures were in the United States?",
+				"a": "6",
+				"src": "http://en.wikipedia.org/wiki/List_of_tallest_structures_in_the_world"
+			},
+			{
+				"q": "What is the wingspan, in meters, of the Boeing 777-200?",
+				"a": 60.9,
+				"src": "http://en.wikipedia.org/wiki/Boeing_777"
+			},
+			{
+				"q": "What was the net worth, in millions of US dollars, of Bill Gates by the end of 2014?",
+				"a": 81000,
+				"src": "http://www.nbcnews.com/business/economy/rich-get-richer-bill-gates-tops-forbes-400-list-richest-n214196"
+			},
+			{
+				"q": "What is the volume, in cubic kilometers, of Lake Superior?",
+				"a": 12100,
+				"src": "http://www.wolframalpha.com/input/?i=lake+superior+volume+in+cubic+meters"
+			},
+			{
+				"q": "In July of 2014, what percentage of US households did not have a landline telephone?",
+				"a": 40,
+				"src": "http://www.chicagotribune.com/business/breaking/chi-landlines-survey-20140708-story.html"
+			},
+			{
+				"q": "How many US adults were unemployed as of November 2014?",
+				"a": 18700000,
+				"src": "http://www.wolframalpha.com/input/?i=%28us+population+november+2014%29+*+%28us+unemployment+november+2014%29"
+			},
+			{
+				"q": "How many deaths were there in the US in 2010?",
+				"a": 2550000,
+				"src": "http://www.wolframalpha.com/input/?i=us+deaths+2010"
+			},
+			{
+				"q": "What was the density, in persons per square mile, of Seattle in 2010?",
+				"a": 7559,
+				"src": "http://www.wolframalpha.com/input/?i=seattle+population+density"
+			},
+			{
+				"q": "What is the land area, in square miles, of Hawaii?",
+				"a": 6423,
+				"src": "http://www.wolframalpha.com/input/?i=hawaii+land+area"
+			},
+			{
+				"q": "In 2010, what percentage of US citizens lived in the state of California?",
+				"a": 12.02,
+				"src": [ "http://www.wolframalpha.com/input/?i=california+population+2010", "http://www.wolframalpha.com/input/?i=united+states+population+2010" ]
+			},
+			{
+				"q": "As of 2014, how many people have been Prime Minister of the United Kingdom?",
+				"a": 75,
+				"src": "http://en.wikipedia.org/wiki/List_of_Prime_Ministers_of_the_United_Kingdom"
+			},
+			{
+				"q": "How many popular votes did Barack Obama receive in the 2008 general election?",
+				"a": 69498516,
+				"src": "http://en.wikipedia.org/wiki/United_States_presidential_election,_2008"
+			},
+			{
+				"q": "In what year was Colorado admitted as a state?",
+				"a": 1876,
+				"src": "http://en.wikipedia.org/wiki/Colorado"
+			},
+			{
+				"q": "What was the cost, in millions of 2014 US dollars, of construction of the Hoover Dam?",
+				"a": 761.46,
+				"src": [ "http://en.wikipedia.org/wiki/Hoover_Dam", "http://www.bls.gov/data/inflation_calculator.htm" ]
+			},
+			{
+				"q": "How long, in centimeters, is the average adult small intestine?",
+				"a": 600,
+				"src": "http://carta.anthropogeny.org/moca/topics/smalllarge-intestine-length-ratio"
+			},
+			{
+				"q": "How fast does human hair grow, in centimeters per year?",
+				"a": 15,
+				"src": "http://en.wikipedia.org/wiki/Human_hair_growth"
+			},
+			{
+				"q": "A blue whale has the equivalent mass of how many average adults?",
+				"a": 2327,
+				"src": [ "http://www.wolframalpha.com/input/?i=average+adult+weight", "https://www.google.com/webhp?q=blue+whale+weight" ]
+			},
+			{
+				"q": "What is the longest diameter of the Milky Way galaxy in light years?",
+				"a": 100000,
+				"src": "http://www.wolframalpha.com/input/?i=milky+way+diameter"
+			},
+			{
+				"q": "How many million solar masses are there in our galaxy?",
+				"a": 100000,
+				"src": "http://www.space.com/25959-how-many-stars-are-in-the-milky-way.html"
+			},
+			{
+				"q": "As of 2014, Denver international airport was the home of the longest airport runway in the United States. How long was it, in miles?",
+				"a": 3.03,
+				"src": "http://en.wikipedia.org/wiki/List_of_longest_runways"
+			},
+			{
+				"q": "On average, how many US commercial airline flights were there per day in 2010?",
+				"a": 29000,
+				"src": "http://www.natca.org/mediacenter/bythenumbers.msp"
+			},
+			{
+				"q": "What is the length, in miles, of the river Nile?",
+				"a": 4258,
+				"src": "http://en.wikipedia.org/wiki/List_of_rivers_by_length"
+			},
+			{
+				"q": "How many new cases of bladder cancer were diagnosed in 2014?",
+				"a": 74690,
+				"src": "http://seer.cancer.gov/statfacts/html/urinb.html"
+			},
+			{
+				"q": "What percentage of patients with breast cancer survive more than five years after initial diagnosis?",
+				"a": 89.2,
+				"src": "http://seer.cancer.gov/statfacts/html/breast.html"
+			},
+			{
+				"q": "The joyo kanji is Japan's Ministry of Education's official list of Chinese characters in everyday use. How many kanji are in this list?",
+				"a": 2136,
+				"src": "http://en.wikipedia.org/wiki/J%C5%8Dy%C5%8D_kanji"
+			},
+			{
+				"q": "In 1998, scientists managed to slow a beam of light to a record minimum. What was the recorded speed, in meters per second?",
+				"a": 17,
+				"src": "http://en.wikipedia.org/wiki/Slow_light"
+			},
+			{
+				"q": "As of 2014, same-sex marriage was fully legal in how many US states (including Washington, D.C.)?",
+				"a": 35,
+				"src": "http://www.freedomtomarry.org/states/"
+			},
+			{
+				"q": "In 2012, what percentage of the United States military active duty force were women?",
+				"a": 14.6,
+				"src": "http://www.militaryonesource.mil/12038/MOS/Reports/2012_Demographics_Report.pdf"
+			},
+			{
+				"q": "How many inches of precipitation did Seattle receive in 2014?",
+				"a": 48.5,
+				"src": "http://www.seattleweatherblog.com/rain-stats/rainfall-2014/"
+			},
+			{
+				"q": "In what year was the World Wide Web Consortium (the W3) founded?",
+				"a": 1994,
+				"src": "http://en.wikipedia.org/wiki/World_Wide_Web_Consortium"
+			},
+			{
+				"q": "How many top-level domains were originally created for the internet, prior to the creation of ICANN in 1998?",
+				"a": 7,
+				"src": "http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains"
+			},
+			{
+				"q": "In 2013, Google acquired GPS navigation software company Waze. How much did Google pay, in US dollars?",
+				"a": 966000000,
+				"src": "http://www.sec.gov/Archives/edgar/data/1288776/000128877613000055/goog10-qq22013.htm"
+			},
+			{
+				"q": "How many distinct breeds of dog are recognized by the American Kennel Club?",
+				"a": 184,
+				"src": "http://www.akc.org/breeds/"
+			},
+			{
+				"q": "What was the median US household income in 2013?",
+				"a": 52250,
+				"src": "http://www.census.gov/content/dam/Census/library/publications/2014/acs/acsbr13-02.pdf"
+			},
+			{
+				"q": "In what year did construction on Seattle's Columbia Tower commence?",
+				"a": 1982,
+				"src": "http://en.wikipedia.org/wiki/Columbia_Center"
+			},
+			{
+				"q": "New York City lists how many individual public tennis courts in the bourough of Manhattan?",
+				"a": 110,
+				"src": "http://www.nycgovparks.org/facilities/tennis"
+			},
+			{
+				"q": "How many total strike-outs were recorded in the 2014 MLB season?",
+				"a": 14270,
+				"src": "http://espn.go.com/mlb/stats/pitching/_/order/false"
+			},
+			{
+				"q": "What is the maximum weight, in ounces, of a regulation NFL football?",
+				"a": 15,
+				"src": "http://static.nfl.com/static/content/public/image/rulebook/pdfs/5_2013_Ball.pdf"
+			},
+			{
+				"q": "What is the seating capacity of Seattle's Safeco Field?",
+				"a": 47476,
+				"src": "http://en.wikipedia.org/wiki/Safeco_Field"
+			},
+			{
+				"q": "How many known species of cat are living in the world today?",
+				"a": 41,
+				"src": "http://en.wikipedia.org/wiki/Felidae"
+			},
+			{
+				"q": "How many entries were in the first edition of the Oxford English Dictionary?",
+				"a": 252200,
+				"src": "http://public.oed.com/history-of-the-oed/dictionary-facts/"
+			},
+			{
+				"q": "What is the radius, in miles, of Earth's inner core?",
+				"a": 760,
+				"src": "http://en.wikipedia.org/wiki/Inner_core"
+			},
+			{
+				"q": "In what year is it believed Shakespeare wrote The Tragedy of Julius Caesar?",
+				"a": 1599,
+				"src": "http://en.wikipedia.org/wiki/Julius_Caesar_(play)"
+			},
+			{
+				"q": "What was the production budget, in millions of dollars, of the video game Grand Theft Auto V?",
+				"a": 265,
+				"src": "http://www.ibtimes.com/gta-5-costs-265-million-develop-market-making-it-most-expensive-video-game-ever-produced-report"
+			},
+			{
+				"q": "How many bills were vetoed by president George W. Bush?",
+				"a": 12,
+				"src": "http://www.senate.gov/reference/Legislation/Vetoes/vetoCounts.htm"
+			},
+			{
+				"q": "How many metric tons of grain molasses cargo were processed by the Port of Seattle in 2010?",
+				"a": 40173,
+				"src": "http://www.portseattle.org/About/Publications/Statistics/Seaport/Pages/10-Year-History.aspx"
+			},
+			{
+				"q": "What percentage of United States high school students dropped out in 2014?",
+				"a": 7,
+				"src": "http://nces.ed.gov/fastfacts/display.asp?id=16"
+			},
+			{
+				"q": "By what percentage did reported Pertussis cases (whooping cough) increase in the US from 2010-2012?",
+				"a": 75.2,
+				"src": "http://www.cdc.gov/pertussis/surv-reporting/cases-by-year.html"
+			}
+		]
+	};
+}
